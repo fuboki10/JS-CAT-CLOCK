f182596c8789f46b3e327b8ce5634f163036b2b6
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-06-22 01:13:31 +0200

    Update Clock Function

--- script.js
+++ script.js
@@ -35,3 +35,55 @@ var showCurrnetTime = function()
 
 	clock.innerText = clockTime;
 }
+
+var updateClock = function()
+{
+	var time = new Date().getHours();
+	var message;
+	var img = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+	var timeEventJS = document.getElementById('timeEvent');
+	var lolCatImageJS = document.getElementById('lolcatImage');
+
+	if (time == partytime)
+  {
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
+    message = "Let's party!";
+  }
+  else if (time == wakeuptime)
+  {
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+    message = "Wake up!";
+  }
+  else if (time == lunchtime)
+  {
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+    message = "Let's have some lunch!";
+  }
+  else if (time == naptime)
+  {
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+    message = "Sleep tight!";
+  }
+  else if (time < noon)
+  {
+    image = "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg";
+    message = "Good morning!";
+  }
+  else if (time >= evening)
+  {
+    image = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cat_sleep.jpg";
+    message = "Good evening!";
+  }
+  else
+  {
+    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+    message = "Good afternoon!";
+  }
+
+  timeEventJS.innerText = message;
+  lolCatImageJS.src = img;
+
+  showCurrnetTime();
+}
+
+updateClock();
